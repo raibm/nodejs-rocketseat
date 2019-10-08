435e25715745bee3cdc19d546caa330a62f405b3
rai.maciel <rai.maciel@basis.com.br>
2019-10-08 14:09:27 -0300

    rota de autenticacao funcional

--- API-NODEJS/src/controllers/authController.js
+++ API-NODEJS/src/controllers/authController.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const User = require('../models/user');
 
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
 //express framework usado para controlar rotas, retorna para o front end jsons, pegar parametros da req, bodys da req etc
 
 const router = express.Router();
@@ -20,6 +22,22 @@ router.post('/register', async (req, res) => {
     }catch( err ){
         return res.status(400).send({ error: 'Registration failed' });
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+   
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+    return res.status(400).send({ error: 'User not found' });
+
+    if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Invalid password' });
+
+    res.send({ user });
+
 });
 
 module.exports = app => app.use('/auth', router); 
